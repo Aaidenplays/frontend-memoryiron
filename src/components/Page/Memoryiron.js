9eea903d0e559076e8dc53d3d5524a4a3bd7903a
jbshipman <jbshipman@icloud.com>
2020-04-16 19:17:43 -0500

    added no default to switch case

--- src/components/Page/Memoryiron.js
+++ src/components/Page/Memoryiron.js
@@ -139,6 +139,7 @@ export default class Memoryiron extends Component {
         console.log('no match');
         this.helperFunctClearStates()
         break;
+        // no default
     };
   };
 
