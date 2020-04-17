cd0f4e84deb4cab45826f1db987fc63930174398
aaidenplays <aaidenplays@yahoo.com>
2020-04-17 10:34:14 -0500

    added final touches

--- src/components/Audios.js
+++ src/components/Audios.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import flipWav from '../audio/240776__f4ngy__card-flip.wav'
+import correct from '../audio/correct.wav'
+import incorrect from '../audio/incorrect.wav'
+import music from '../audio/MemoryIron-Ketchup.wav'
 
 
 export class Audios extends Component {
@@ -8,6 +11,15 @@ export class Audios extends Component {
       <div>
         <audio className='audio-element'>
           <source src={flipWav}></source>
+        </audio>
+        <audio className='audio-correct'>
+          <source src={correct}></source>
+        </audio>
+        <audio className='audio-incorrect'>
+          <source src={incorrect}></source>
+        </audio>
+        <audio className='audio-music'>
+          <source src={music}></source>
         </audio>
       </div>
     )
--- src/components/Page/Memoryiron.js
+++ src/components/Page/Memoryiron.js
@@ -1,14 +1,13 @@
-import React, { Component } from "react";
-import Header from './Header';
-import UtilityBar from './UtilityBar';
-import MainCanvas from './MainCanvas';
-import { Container } from 'semantic-ui-react';
+import React, { Component } from 'react'
+import Header from './Header'
+import UtilityBar from './UtilityBar'
+import MainCanvas from './MainCanvas'
+import { Container } from 'semantic-ui-react'
 
-
-const imgAPIURL = 'http://localhost:3001/cards';
+const imgAPIURL = 'http://localhost:3001/cards'
 
 export default class Memoryiron extends Component {
-  constructor() {
+  constructor () {
     super()
     this.state = {
       gameDeck: [],
@@ -19,136 +18,182 @@ export default class Memoryiron extends Component {
       isMatch: false,
       score: 0,
       holdFront: false
-    };
-  };
+    }
+  }
 
-  componentDidMount() {
+  componentDidMount () {
     fetch(imgAPIURL)
       .then(res => res.json())
       // .then(gameDeck => console.log(gameDeck))
       .then(gameDeck => this.setState({ gameDeck }))
-  };
+  }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.secondCard !== this.state.secondCard && this.state.secondCard !== 0) {
-      this.cardMatch();
-    };
-  };
+  componentDidUpdate (prevProps, prevState) {
+    if (
+      prevState.secondCard !== this.state.secondCard &&
+      this.state.secondCard !== 0
+    ) {
+      this.cardMatch()
+    }
+  }
 
-  handleSelect = (selection) => {
+  handleSelect = selection => {
     if (selection === '1') {
       this.nullGame()
     } else if (selection === '4') {
+      this.playMusicAudio()
       this.fourByFour()
     } else if (selection === '6') {
+      this.playMusicAudio()
       this.sixBySix()
     } else if (selection === '8') {
+      this.playMusicAudio()
       this.eightByEight()
-    };
-  };
+    }
+  }
 
   helperFunctClearStates = () => {
     this.setState({ firstCard: 0 })
     this.setState({ secondCard: 0 })
     this.setState({ isMatch: false })
-  };
+  }
 
   nullGame = () => {
     this.setState({ game: [] })
     this.setState({ gridSize: 1 })
     this.helperFunctClearStates()
     this.setState({ score: 0 })
-  };
+  }
 
   fourByFour = () => {
-    let shuffleCards = this.state.gameDeck.sort(() => Math.random() - 0.5);
-    let selectedCardsA = shuffleCards.slice(0, 8);
-    let selectedCardsB = shuffleCards.slice(0, 8);
+    let shuffleCards = this.state.gameDeck.sort(() => Math.random() - 0.5)
+    let selectedCardsA = shuffleCards.slice(0, 8)
+    let selectedCardsB = shuffleCards.slice(0, 8)
 
-    selectedCardsB.map(card => (
-      selectedCardsA.push(card)
-    ))
+    selectedCardsB.map(card => selectedCardsA.push(card))
 
-    let game = selectedCardsA.sort(() => Math.random() - 0.5);
+    let game = selectedCardsA.sort(() => Math.random() - 0.5)
     this.setState({ game })
     this.setState({ gridSize: 4 })
     this.setState({ score: 0 })
     this.helperFunctClearStates()
-  };
+  }
 
   sixBySix = () => {
-    let shuffleCards = this.state.gameDeck.sort(() => Math.random() - 0.5);
-    let selectedCardsA = shuffleCards.slice(0, 18);
-    let selectedCardsB = shuffleCards.slice(0, 18);
+    let shuffleCards = this.state.gameDeck.sort(() => Math.random() - 0.5)
+    let selectedCardsA = shuffleCards.slice(0, 18)
+    let selectedCardsB = shuffleCards.slice(0, 18)
 
-    selectedCardsB.map(card => (
-      selectedCardsA.push(card)
-    ))
+    selectedCardsB.map(card => selectedCardsA.push(card))
 
-    let game = selectedCardsA.sort(() => Math.random() - 0.5);
+    let game = selectedCardsA.sort(() => Math.random() - 0.5)
     this.setState({ game })
     this.setState({ gridSize: 6 })
     this.setState({ score: 0 })
     this.helperFunctClearStates()
-  };
+  }
 
   eightByEight = () => {
-    let shuffleCards = this.state.gameDeck.sort(() => Math.random() - 0.5);
-    let selectedCardsA = shuffleCards.slice(0, 32);
-    let selectedCardsB = shuffleCards.slice(0, 32);
+    let shuffleCards = this.state.gameDeck.sort(() => Math.random() - 0.5)
+    let selectedCardsA = shuffleCards.slice(0, 32)
+    let selectedCardsB = shuffleCards.slice(0, 32)
 
-    selectedCardsB.map(card => (
-      selectedCardsA.push(card)
-    ))
+    selectedCardsB.map(card => selectedCardsA.push(card))
 
-    let game = selectedCardsA.sort(() => Math.random() - 0.5);
+    let game = selectedCardsA.sort(() => Math.random() - 0.5)
 
     this.setState({ game })
     this.setState({ gridSize: 8 })
     this.setState({ score: 0 })
     this.helperFunctClearStates()
-  };
+  }
 
-  cardChoice = (card) => {
+  playMusicAudio = () => {
+    const audioEl = document.getElementsByClassName('audio-music')[0]
+    audioEl.play()
+  }
+
+  playCorrectAudio = () => {
+    const audioEl = document.getElementsByClassName('audio-correct')[0]
+    audioEl.play()
+  }
+  playIncorrectAudio = () => {
+    const audioEl = document.getElementsByClassName('audio-incorrect')[0]
+    audioEl.play()
+  }
+
+  cardChoice = card => {
     switch (true) {
       case this.state.firstCard === 0 && this.state.secondCard === 0:
-        let firstCard = card.id;
+        let firstCard = card.id
         // console.log(firstCard);
-        this.setState({ firstCard });
-        break;
+        this.setState({ firstCard })
+        break
       case this.state.firstCard > 0 && this.state.secondCard === 0:
-        let secondCard = card.id;
+        let secondCard = card.id
         // console.log(secondCard);
-        this.setState({ secondCard });
-        break;
+        this.setState({ secondCard })
+        break
       // no default
-    };
-  };
+    }
+  }
 
   cardMatch = () => {
     console.log('do these match?')
     switch (true) {
       case this.state.firstCard === this.state.secondCard:
-        this.setState({ isMatch: true });
-        let score = this.state.score + 2;
-        this.setState({ score });
-        console.log('match');
-        this.helperFunctClearStates();
-        break;
-      case this.state.firstCard !== this.state.secondCard:
-        console.log('no match');
+        {
+          this.playCorrectAudio()
+        }
+        this.setState({ isMatch: true })
+        let score = this.state.score + 2
+        this.setState({ score })
+        console.log('match')
         this.helperFunctClearStates()
-        break;
-        // no default
-    };
-  };
+        break
+      case this.state.firstCard !== this.state.secondCard:
+        {
+          this.playIncorrectAudio()
+        }
+        console.log('no match')
+        // this.autoFlip(this.state.firstCard, this.state.secondCard)
+        this.helperFunctClearStates()
+        break
+      // no default
+    }
+  }
 
-  render() {
+  // autoFlip = (card1, card2) => {
+  //   card1 = this.state.gameDeck.filter(card => {
+  //     return card.id === card1
+  //   })
+  //   console.log('CARD1:::', card1)
+  //   card2 = this.state.gameDeck.filter(card => {
+  //     return card.id === card2
+  //   })
+  //   console.log('CARD2:::', card2)
+  //   if (card1.length > 0 && card2 > 0) {
+  //     console.log('YOU MADE IT')
+  //     card1.first.setState({
+  //       isFlipped: false
+  //     })
+  //     console.log('YOU MADE IT')
+  //     card2.first.setState({
+  //       isFlipped: false
+  //     })
+  //   }
+  // }
+
+  render () {
     return (
       <Container>
         <div>
           <Header user={this.props.user} handleSelect={this.handleSelect} />
-          <UtilityBar handleSelect={this.handleSelect} game={this.state.gameDeck} user={this.props.user} />
+          <UtilityBar
+            handleSelect={this.handleSelect}
+            game={this.state.gameDeck}
+            user={this.props.user}
+          />
           <MainCanvas
             handleSelect={this.handleSelect}
             game={this.state.game}
@@ -161,5 +206,5 @@ export default class Memoryiron extends Component {
         </div>
       </Container>
     )
-  };
-};
+  }
+}
