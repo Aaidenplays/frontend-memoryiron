f397a57de72b4d5a3f7b3caf931db61540c0f178
jbshipman <jbshipman@icloud.com>
2020-04-16 22:09:36 -0500

    move User name and score to Utility bar

--- src/components/Page/UserScores.js
+++ src/components/Page/UserScores.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
 export default class UserScores extends Component {
+
+  totalScore = (score) => {
+    return score = ((score / 2) * 3.1415) / 10
+  };
+
   render() {
     return (
       <div>
-        <div>{this.props.user.username}</div>
+        {this.props.user ? (
+          <div>
+            {this.props.user.username}, your current score is:<br />
+            {this.totalScore(this.props.score)}
+          </div>
+        ) : null}
+
       </div>
     )
   }
